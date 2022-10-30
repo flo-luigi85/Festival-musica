377338967a7ab9f97ec7d23eec7fe7aabc72425b
flo-luigi85 <flo.lugi85@gmail.com>
2022-10-30 11:22:53 -0300

    modificando scss js

--- build/js/app.js
+++ build/js/app.js
@@ -3,12 +3,36 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 function iniciarApp() {
+  navegacionFija();
   crearGaleria();
+  scrollNav();
+}
+
+function navegacionFija() {
+  const barra = document.querySelector(".header");
+  const sobreFestival = document.querySelector(".sobre-festival");
+
+  window.addEventListener("scroll", function () {
+    console.log( sobreFestival.getBoundingClientRect() );
+  });
+}
+
+function scrollNav() {
+  const enlaces = document.querySelectorAll(".navegacion-principal a");
+
+  enlaces.forEach((enlace) => {
+    enlace.addEventListener("click", function (e) {
+      e.preventDefault();
+      const seccionScroll = e.target.attributes.href.value;
+      const seccion = document.querySelector(seccionScroll);
+      seccion.scrollIntoView({ behavior: "smooth" });
+    });
+  });
 }
 
 function crearGaleria() {
   const galeria = document.querySelector(".galeria-imagenes");
-  /* galeria.textContent = 'Vamos crear la galería'; */
+  /* galeria.textContent = 'Creando la galería'; */
   for (let i = 1; i <= 12; i++) {
     const imagen = document.createElement("picture");
     imagen.innerHTML = `
@@ -17,27 +41,47 @@ function crearGaleria() {
         <img loading="lazy" width="200" height="300" src="build/img/thumb/${i}.jpg" alt="Imagen galeria">
         `;
 
-        //Asignando un evento | igual a un callback (es parar pasar un parametro)
-        imagen.onclick = function(){
-            mostrarImagen(i);
-        }
+    //Asignando un evento | igual a un callback (es parar pasar un parametro)
+    imagen.onclick = function () {
+      mostrarImagen(i);
+    };
 
     galeria.appendChild(imagen);
   }
 }
 
-function mostrarImagen(id){
-    const imagen = document.createElement("picture");
-    imagen.innerHTML = `
+function mostrarImagen(id) {
+  const imagen = document.createElement("picture");
+  imagen.innerHTML = `
         <source srcset="build/img/grande/${id}.avif" type="image/avif">
         <source srcset="build/img/grande/${id}.webp" type="image/webp">
         <img loading="lazy" width="200" height="300" src="build/img/grande/${id}.jpg" alt="Imagen galeria">
         `;
 
-        const overlay = document.createElement('DIV');
-        overlay.appendChild(imagen);
-        overlay.classList.add('overlay');
+  // Crea el overlay con la imagen
+  const overlay = document.createElement("DIV");
+  overlay.appendChild(imagen);
+  overlay.classList.add("overlay");
+  overlay.onclick = function () {
+    const body = document.querySelector("body");
+    body.classList.remove("fijar-body");
+    overlay.remove();
+  };
 
-        const body = document.querySelector('body');
-        body.appendChild(overlay);
+  //  Boton para cerrar el Modal
+  const cerrarModal = document.createElement("p");
+  cerrarModal.textContent = "X";
+  cerrarModal.classList.add("btn-cerrar");
+  //otro calback
+  cerrarModal.onclick = function () {
+    const body = document.querySelector("body");
+    body.classList.remove("fijar-body");
+    overlay.remove();
+  };
+  overlay.appendChild(cerrarModal);
+
+  //Añadirlo al HTML
+  const body = document.querySelector("body");
+  body.appendChild(overlay);
+  body.classList.add("fijar-body");
 }
